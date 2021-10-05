1b1d9923f5be397e3ae604cd242cffda4a11190d
aagneess <70575715+aagneess@users.noreply.github.com>
2021-10-05 14:53:16 +0200

    first commit

---
+++ public/themes/wordplate/footer.php
@@ -0,0 +1,7 @@
+<h1>footer</h1>
+
+<script src="<?= get_theme_file_uri('assets/app.js') ?>" async></script>
+<?php wp_footer(); ?>
+</body>
+
+</html>
---
+++ public/themes/wordplate/index.php
@@ -0,0 +1,17 @@
+<?php get_header(); ?>
+
+<main role="main">
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+            <article>
+
+                <?php the_content(); ?>
+            </article>
+        <?php endwhile;
+    else : ?>
+        <article>
+            <p>Nothing to see.</p>
+        </article>
+    <?php endif; ?>
+</main>
+
+<?php get_footer(); ?>
